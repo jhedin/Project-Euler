ea4972f592700ee79eff1398c9254c1e689273e1
James Hedin <j.hedin10@gmail.com>
2015-12-08 01:41:29 -0800

    solves problem 5

---
+++ Problem-5.js
@@ -0,0 +1,11 @@
+
+// smallest multiple of numbers from 1 to 20
+
+// prime factors
+
+// 2,3,5,7,11,13,17,19
+
+// what's the lagest power of each prime <= 20? (easy todo by hand)
+
+console.log(16 * 9 * 5 * 7 * 11 * 13 * 17 * 19);
+
