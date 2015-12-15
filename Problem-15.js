e37be49f1db3aa067ff90019406babcbdc0ee841
James Hedin <j.hedin10@gmail.com>
2015-12-14 22:20:05 -0800

    Solves Problem 15 quick and dirty

--- Problem-15.js
+++ Problem-15.js
@@ -1,13 +1,27 @@
 
+var cache = [];
+for(var i = 0; i < 20; i++) {
+	cache.push([]);
+	for(var j = 0; j < 20; j++)
+		cache[i].push(0);
+}
 
 function f(x,y) {
+	if(cache[x-1][y-1] != 0) {
+		return cache[x-1][y-1];
+	}
+
 	if(y == 1) {
 		return x+1;
 	}
 	if(x == 1) {
 		return y+1;
 	}
-	return f(x,y-1) + f(x-1,y);
+	var res = f(x,y-1) + f(x-1,y);
+
+	cache[x-1][y-1] = res;
+	cache[y-1][x-1] = res;
+	return res;
 }
 
 console.log(f(20,20));
